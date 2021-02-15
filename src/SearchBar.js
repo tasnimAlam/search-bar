ac2ab55c25e1690e4fe86b2e04d90ff37314af79
tasnimAlam <tasnimalamcse@gmail.com>
2021-02-15 21:10:15 +0600

    Use Icons from react-icons

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,14 +1,21 @@
-import React from "react";
-import PropTypes from "prop-types";
-import IconButton from "@material-ui/core/IconButton";
-import Input from "@material-ui/core/Input";
-import Paper from "@material-ui/core/Paper";
-import ClearIcon from "@material-ui/icons/Clear";
-import SearchIcon from "@material-ui/icons/Search";
-import withStyles from "@material-ui/core/styles/withStyles";
-import classNames from "classnames";
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+  cloneElement,
+} from "react"
+import PropTypes from "prop-types"
+import IconButton from "@material-ui/core/IconButton"
+import Input from "@material-ui/core/Input"
+import Paper from "@material-ui/core/Paper"
+import { MdSearch, MdClear } from "react-icons/md"
+import withStyles from "@material-ui/core/styles/withStyles"
+import classNames from "classnames"
 
-const styles = (theme) => ({
+const styles = theme => ({
   root: {
     height: theme.spacing(6),
     display: "flex",
@@ -44,13 +51,9 @@ const styles = (theme) => ({
     margin: "auto 16px",
     width: `calc(100% - ${theme.spacing(6 + 4)}px)`, // 6 button + 4 margin
   },
-});
+})
 
-/**
- * Material design search bar
- * @see [Search patterns](https://material.io/archive/guidelines/patterns/search.html)
- */
-const SearchBar = React.forwardRef(
+const SearchBar = forwardRef(
   (
     {
       cancelOnEscape,
@@ -66,80 +69,80 @@ const SearchBar = React.forwardRef(
     },
     ref
   ) => {
-    const inputRef = React.useRef();
-    const [value, setValue] = React.useState(inputProps.value);
+    const inputRef = useRef()
+    const [value, setValue] = useState(inputProps.value)
 
-    React.useEffect(() => {
-      setValue(inputProps.value);
-    }, [inputProps.value]);
+    useEffect(() => {
+      setValue(inputProps.value)
+    }, [inputProps.value])
 
-    const handleFocus = React.useCallback(
-      (e) => {
+    const handleFocus = useCallback(
+      e => {
         if (inputProps.onFocus) {
-          inputProps.onFocus(e);
+          inputProps.onFocus(e)
         }
       },
       [inputProps.onFocus]
-    );
+    )
 
-    const handleBlur = React.useCallback(
-      (e) => {
-        setValue((v) => v.trim());
+    const handleBlur = useCallback(
+      e => {
+        setValue(v => v.trim())
         if (inputProps.onBlur) {
-          inputProps.onBlur(e);
+          inputProps.onBlur(e)
         }
       },
       [inputProps.onBlur]
-    );
+    )
 
-    const handleInput = React.useCallback(
-      (e) => {
-        setValue(e.target.value);
+    const handleInput = useCallback(
+      e => {
+        setValue(e.target.value)
         if (inputProps.onChange) {
-          inputProps.onChange(e.target.value);
+          inputProps.onChange(e.target.value)
         }
       },
       [inputProps.onChange]
-    );
+    )
 
-    const handleCancel = React.useCallback(() => {
-      setValue("");
+    const handleCancel = useCallback(() => {
+      setValue("")
       if (onCancelSearch) {
-        onCancelSearch();
+        onCancelSearch()
       }
-    }, [onCancelSearch]);
+    }, [onCancelSearch])
 
-    const handleRequestSearch = React.useCallback(() => {
+    const handleRequestSearch = useCallback(() => {
       if (onRequestSearch) {
-        onRequestSearch(value);
+        onRequestSearch(value)
       }
-    }, [onRequestSearch, value]);
+    }, [onRequestSearch, value])
 
-    const handleKeyUp = React.useCallback(
-      (e) => {
+    const handleKeyUp = useCallback(
+      e => {
         if (e.charCode === 13 || e.key === "Enter") {
-          handleRequestSearch();
+          handleRequestSearch()
         } else if (
           cancelOnEscape &&
           (e.charCode === 27 || e.key === "Escape")
         ) {
-          handleCancel();
+          handleCancel()
         }
         if (inputProps.onKeyUp) {
-          inputProps.onKeyUp(e);
+          inputProps.onKeyUp(e)
         }
       },
       [handleRequestSearch, cancelOnEscape, handleCancel, inputProps.onKeyUp]
-    );
+    )
 
-    React.useImperativeHandle(ref, () => ({
+    useImperativeHandle(ref, () => ({
       focus: () => {
-        inputRef.current.focus();
+        inputRef.current.focus()
       },
       blur: () => {
-        inputRef.current.blur();
+        inputRef.current.blur()
       },
-    }));
+    }))
 
     return (
       <Paper className={classNames(classes.root, className)} style={style}>
@@ -165,7 +168,7 @@ const SearchBar = React.forwardRef(
           })}
           disabled={disabled}
         >
-          {React.cloneElement(searchIcon, {
+          {cloneElement(searchIcon, {
             classes: { root: classes.icon },
           })}
         </IconButton>
@@ -176,50 +179,38 @@ const SearchBar = React.forwardRef(
           })}
           disabled={disabled}
         >
-          {React.cloneElement(closeIcon, {
+          {cloneElement(closeIcon, {
             classes: { root: classes.icon },
           })}
         </IconButton>
       </Paper>
-    );
+    )
   }
-);
+)
 
 SearchBar.defaultProps = {
   className: "",
-  closeIcon: <ClearIcon />,
+  closeIcon: <MdClear />,
   disabled: false,
-  placeholder: "Search",
-  searchIcon: <SearchIcon />,
+  placeholder: "Enter Text",
+  searchIcon: <MdSearch />,
   style: null,
   value: "",
-};
+}
 
 SearchBar.propTypes = {
-  /** Whether to clear search on escape */
   cancelOnEscape: PropTypes.bool,
-  /** Override or extend the styles applied to the component. */
   classes: PropTypes.object.isRequired,
-  /** Custom top-level class */
   className: PropTypes.string,
-  /** Override the close icon. */
   closeIcon: PropTypes.node,
-  /** Disables text field. */
   disabled: PropTypes.bool,
-  /** Fired when the search is cancelled. */
   onCancelSearch: PropTypes.func,
-  /** Fired when the text value changes. */
   onChange: PropTypes.func,
-  /** Fired when the search icon is clicked. */
   onRequestSearch: PropTypes.func,
-  /** Sets placeholder text for the embedded text field. */
   placeholder: PropTypes.string,
-  /** Override the search icon. */
   searchIcon: PropTypes.node,
-  /** Override the inline-styles of the root element. */
   style: PropTypes.object,
-  /** The value of the text field. */
   value: PropTypes.string,
-};
+}
 
-export default withStyles(styles)(SearchBar);
+export default withStyles(styles)(SearchBar)
